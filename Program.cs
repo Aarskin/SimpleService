d78950542378b6df3540ec4f43618292b25e9277
Matthew Wiemer <matthew.wiemer@relativity.com>
2018-08-06 10:51:45 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+
+namespace SimpleWebApp
+{
+	class Program
+	{
+		public const string BASE_ADDRESS = "http://localhost:8385/";
+		static void Main(string[] args)
+		{
+			using (WebApp.Start<OwinSelfHost>(BASE_ADDRESS))
+			{
+				Console.WriteLine($"Simple service listening at {BASE_ADDRESS}...");
+
+				Console.ReadKey();
+			}
+		}
+	}
+}
---
+++ SimpleController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace SimpleWebApp
+{
+	public class SimpleController : ApiController
+	{
+		// GET api/values 
+		public IEnumerable<string> Get()
+		{
+			return new string[] { DateTime.Now.ToString(), "foo", "bar" };
+		}
+
+		// GET api/values/5 
+		public string Get(int id)
+		{
+			return $"foobar_{id}";
+		}
+
+		// POST api/values 
+		public void Post([FromBody]string value)
+		{
+			throw new NotImplementedException("POST NOT IMPLEMENTED");
+		}
+
+		// PUT api/values/5 
+		public void Put(int id, [FromBody]string value)
+		{
+			throw new NotImplementedException("PUT NOT IMPLEMENTED");
+		}
+
+		// DELETE api/values/5 
+		public void Delete(int id)
+		{
+			throw new NotImplementedException("DELETE NOT IMPLEMENTED");
+		}
+	}
+}
